29517abf871134af1ab49c2eb45c4ba6e6923ad2
raisa098 <115281327+raisa098@users.noreply.github.com>
2022-10-17 16:13:06 +0700

    Update inline.py

--- NIXA/inline.py
+++ NIXA/inline.py
@@ -11,8 +11,8 @@ from config import GROUP_SUPPORT, UPDATES_CHANNEL
 def stream_markup(user_id):
   buttons = [
     [
-      InlineKeyboardButton(text="• ᴍᴇɴᴜ", callback_data=f'cbmenu | {user_id}'),
-      InlineKeyboardButton(text="• ᴄʟᴏsᴇ", callback_data=f'cls'),
+      InlineKeyboardButton(text="•ᴍᴇɴᴜ", callback_data=f'cbmenu | {user_id}'),
+      InlineKeyboardButton(text="•ᴄʟᴏsᴇ", callback_data=f'cls'),
     ],
     [
       InlineKeyboardButton(text="🥂 ɢʀᴏᴜᴘ", url=f"https://t.me/{GROUP_SUPPORT}"),
@@ -30,11 +30,11 @@ def menu_markup(user_id):
       InlineKeyboardButton(text="▷", callback_data=f'cbresume | {user_id}'),
     ],
     [
-      InlineKeyboardButton(text="• ᴍᴜᴛᴇ •", callback_data=f'cbmute | {user_id}'),
-      InlineKeyboardButton(text="• ᴜɴᴍᴜᴛᴇ •", callback_data=f'cbunmute | {user_id}'),
+      InlineKeyboardButton(text="•ᴍᴜᴛᴇ•", callback_data=f'cbmute | {user_id}'),
+      InlineKeyboardButton(text="•ᴜɴᴍᴜᴛᴇ•", callback_data=f'cbunmute | {user_id}'),
     ],
     [
-      InlineKeyboardButton(text="• ᴄʟᴏsᴇ •", callback_data='cls'),
+      InlineKeyboardButton(text="•ᴄʟᴏsᴇ•", callback_data='cls'),
     ]
   ]
   return buttons
@@ -44,7 +44,7 @@ close_mark = InlineKeyboardMarkup(
   [
     [
       InlineKeyboardButton(
-        "• ᴄʟᴏsᴇ •", callback_data="cls"
+        "•ᴄʟᴏsᴇ•", callback_data="cls"
       )
     ]
   ]
@@ -55,7 +55,7 @@ back_mark = InlineKeyboardMarkup(
   [
     [
       InlineKeyboardButton(
-        "• ɢᴏ ʙᴀᴄᴋ •", callback_data="cbmenu"
+        "•ɢᴏ ʙᴀᴄᴋ•", callback_data="cbmenu"
       )
     ]
   ]
