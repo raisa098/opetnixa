1e9ef1449e0ce828676ae28288a9fdb26708196b
raisa098 <115281327+raisa098@users.noreply.github.com>
2022-10-17 16:09:14 +0700

    Update play.py

--- NIXA/Player/play.py
+++ NIXA/Player/play.py
@@ -256,8 +256,8 @@ async def play(c: Client, m: Message):
                       reply_markup=InlineKeyboardMarkup(
                     [
                         [
-                            InlineKeyboardButton("👥 sᴜᴘᴘᴏʀᴛ", url="https://t.me/TechQuardSupport"),
-                            InlineKeyboardButton("📢 ᴜᴘᴅᴀᴛᴇs", url="https://t.me/TechQuard")
+                            InlineKeyboardButton("👥 sᴜᴘᴘᴏʀᴛ", url="https://t.me/pbm212"),
+                            InlineKeyboardButton("📢 ᴜᴘᴅᴀᴛᴇs", url="https://t.me/windtalkerr")
                         ],
                         [
                             InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="cls")
